5b07704950c73d65381ef64bd6fbe5dea9556ae1
Daniel Wylie <daniel@mindscape.co.nz>
2016-02-05 10:16:32 +1300

    VS2013

---
+++ src/TestApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using HipchatApiV2;
+using HipchatApiV2.Enums;
+using HipchatApiV2.Requests;
+
+namespace TestApplication
+{
+  internal class Program
+  {
+    private static void Main()
+    {
+      HipchatClient hipchatClient = new HipchatClient("1KoQTmxVnWZDcHObCfPKKKvMnR9msOlX4Wn54fJG");
+
+      var r = hipchatClient.SendNotification(2259497, new SendRoomNotificationRequest
+      {
+        MessageFormat = HipchatMessageFormat.Text,
+        Message = "Hello :) (yey)",
+        Color = RoomColors.Green,
+        Notify = true
+      });
+    }
+  }
+}
